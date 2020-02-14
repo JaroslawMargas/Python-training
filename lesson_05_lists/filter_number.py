5b11da8dc48117106b84dffd1326c40b1714c4d9
jaromarg@gmail.com <jaromarg@gmail.com>
2020-02-14 14:37:47 +0100

    second

--- lesson_05_lists/filter_number.py
+++ lesson_05_lists/filter_number.py
@@ -4,4 +4,18 @@
 
 list_of_strings = ['2', '0', '8', '3']
 
-print([int(x) for x in list_of_strings if int(x) not in range(3)])
+print([str(x) for x in list_of_strings if int(x) not in range(3)])
+
+# filter usage
+val = 3
+def function(arg):
+    if int(arg) >= val:
+        return True
+    else:
+        return False
+
+
+print(list(filter(function, list_of_strings)))
+
+# lambda usage
+print(list(filter(lambda x: int(x) >= val, list_of_strings)))

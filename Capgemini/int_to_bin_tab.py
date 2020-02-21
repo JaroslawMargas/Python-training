88c6b272466e68f52e8d2ca97af271c71710586d
BABA <jaromarg@gmail.com>
2020-02-21 14:37:06 +0100

    2nd

--- Capgemini/int_to_bin_tab.py
+++ Capgemini/int_to_bin_tab.py
@@ -8,7 +8,7 @@ def check_n(val):
     while True:
         if 2 ** i > val:
             max_factor = i
-            return max_factor-1
+            return max_factor - 1
         else:
             i += 1
 
@@ -16,6 +16,7 @@ def check_n(val):
 factor = check_n(value)
 bin_tab = []
 sum_bin = 0
+bin_tab2 = []
 
 for x in range(factor, -1, -1):
     tmp = 2 ** x
@@ -25,3 +26,17 @@ for x in range(factor, -1, -1):
     else:
         bin_tab.append(0)
 print(bin_tab)
+
+#####
+
+while True:
+    while value > 0:
+        if value % 2 == 0:
+            bin_tab2.append(0)
+        else:
+            bin_tab2.append(1)
+        value = int(value // 2)
+    else:
+        bin_tab2 = bin_tab2[::-1]
+        print(bin_tab2)
+        break

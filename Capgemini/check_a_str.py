431348555dc33fc7a9bb348f6dd906af85afdf54
JaroslawMargas <42811636+JaroslawMargas@users.noreply.github.com>
2020-02-27 18:33:27 +0100

    Update check_a_str.py

--- Capgemini/check_a_str.py
+++ Capgemini/check_a_str.py
@@ -10,4 +10,4 @@ result = re.findall(exp, string)
 if len(result) > 1:
     print("There are: {0} chars {1}".format(str(len(result)), reg))
 else:
-    print(" Does not exist")
+    print("NO more than one")
